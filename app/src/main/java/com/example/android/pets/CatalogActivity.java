60a448ea56ea1a760c618100de3a2cd0acd24a0c
RalphM <36464444+lakerfan32@users.noreply.github.com>
2018-09-07 22:24:38 -0400

    -Add URIs to PetContract -Setup URI Matcher in PetProvider -Replace query method in PetProvider

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -86,15 +86,14 @@ public class CatalogActivity extends AppCompatActivity {
                 PetEntry.COLUMN_PET_GENDER,
                 PetEntry.COLUMN_PET_WEIGHT };
 
-        // Perform a query on the pets table
-        Cursor cursor = db.query(
-                PetEntry.TABLE_NAME,   // The table to query
-                projection,            // The columns to return
-                null,          // The columns for the WHERE clause
-                null,       // The values for the WHERE clause
-                null,          // Don't group the rows
-                null,           // Don't filter by row groups
-                null);         // The sort order
+        // Perform a query on the provider using the ContentResolver..
+        // Use the {@link PetEntry#CONTENT_URI} to access the pet data.
+        Cursor cursor = getContentResolver().query(
+                PetEntry.CONTENT_URI,   // The content URI
+                projection,             // The columns to return for each row
+                null,           // Selection criteria
+                null,        // Selection criteria
+                null);         // The sort order for the returned rows
 
         TextView displayView = (TextView) findViewById(R.id.text_view_pet);
 
